7bd8dde4dbebae2990a272437ec3eafb74006261
Boutaina <boutaina.elatbaoui@gmail.com>
2023-10-12 19:45:32 +0100

    Added the logic to save an employee

--- src/main/java/com/example/companymanagement/HelloServlet.java
+++ src/main/java/com/example/companymanagement/HelloServlet.java
@@ -1,28 +1,31 @@
 package com.example.companymanagement;
 
 import jakarta.persistence.*;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import model.Employee;
+import service.EmployeeService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet", loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
     private String message;
     private EntityManager entityManager;
     private EntityManagerFactory entityManagerFactory;
+    private EmployeeService employeeService;
 
     public void init() {
         message = "Hello World!";
         entityManagerFactory = Persistence.createEntityManagerFactory("default");
         entityManager = entityManagerFactory.createEntityManager();
+        employeeService = new EmployeeService(entityManager);
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
@@ -32,6 +35,17 @@ public class HelloServlet extends HttpServlet {
         } else {
             out.println("<p>Failed to connect to the database.</p>");
         }
+
+        Employee employee = new Employee();
+        employee.setFullName("Joe");
+        employee.setEmail("joe@gmail.com");
+        employee.setPassword("123 Main St");
+        employee.setTelephone("555-123-4567");
+        employee.setSalary(50000.0);
+
+        employeeService.createEmployee(employee);
+
+        out.println("<p>Employee created and saved to the database.</p>");
 
         entityManager.close();
         entityManagerFactory.close();
